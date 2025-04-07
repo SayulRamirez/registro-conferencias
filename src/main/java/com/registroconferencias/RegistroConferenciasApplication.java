d3daf086a0c7115c038837e471e31b610c398655
SayulRamirez <sayulramirez@gmail.com>
2025-04-06 22:00:47 -0600

    creando el proyecto: plataforma de registro para conferencias online

---
+++ src/main/java/com/registroconferencias/RegistroConferenciasApplication.java
@@ -0,0 +1,13 @@
+package com.registroconferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroConferenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroConferenciasApplication.class, args);
+    }
+
+}
