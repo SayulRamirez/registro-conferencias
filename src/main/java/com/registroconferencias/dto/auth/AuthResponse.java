bd3619f702d536897ef8eac602d3519b2197a420
SayulRamirez <sayulramirez@gmail.com>
2025-04-13 15:01:00 -0600

    refactor: se agrego el record AuthResponse para devolver el token, se modificaron las validaciones de LoginRequest y se elimino LoginResponse

---
+++ src/main/java/com/registroconferencias/dto/auth/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.registroconferencias.dto.auth;
+
+public record AuthResponse (String token){
+}
--- src/main/java/com/registroconferencias/dto/auth/LoginResponse.java
+++
@@ -1,3 +0,0 @@
-package com.registroconferencias.dto.auth;
-
-public record LoginResponse (Long id, String rol){}
